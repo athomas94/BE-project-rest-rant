5e63c5c1d454d83eee1346469a52b30dea459707
Andrew Thomas <athomas94@gmail.com>
2024-01-20 19:40:57 -0500

    added readme and corrected controller path

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project REST-rant
+
+REST-Rant is an app where users can review restaurants.
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 
-app.use('places', require('./controllers/places'))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', function (req, res) {
     res.send ('Hello world!')
