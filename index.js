80e7355c4ba85f62a62904de9445d5f752d96ce7
Andrew Thomas <athomas94@gmail.com>
2024-01-20 19:25:19 -0500

    adding 404 page and status

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ app.get('/', function (req, res) {
     res.send ('Hello world!')
 })
 
+app.get ('*', function (req, res) {
+    res.status(404).send ('<h1>404 page</h1>')
+})
 app.listen(process.env.PORT)
