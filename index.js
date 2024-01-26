25a82724598b1824171dd82724cd9cbb351a048f
Andrew Thomas <athomas94@gmail.com>
2024-01-25 19:37:07 -0500

    added delete and edit paths

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+//Globals
 const express = require('express');
 const app = express();
 require('dotenv').config()
+const methodOverride = require('method-override')
 
+//Express settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', function (req, res) {
     res.render('home')
