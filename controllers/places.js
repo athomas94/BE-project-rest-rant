2eae168d41faf5507b8a89a5cb2da80f38876ef3
Andrew Thomas <athomas94@gmail.com>
2024-01-25 20:27:03 -0500

    added completed edit path

--- controllers/places.js
+++ controllers/places.js
@@ -23,9 +23,33 @@ router.get('/:id/edit', (req, res) => {
      res.render('error404')
     }
     else {
-     res.render('places/edit', {place: places[id]})
+     res.render('places/edit', {place: places[id], id})
     }
  })
+
+router.put('/:id', (req, res) => {
+let id = Number(req.params.id)
+if (isNaN(id)) {
+    res.render('error404')
+}
+else if  (!places[id]) {
+    res.render('error404')
+}
+else {
+    if (!req.body.city) {
+        req.body.city='Andyville'
+    }
+    if (!req.body.state) {
+        req.body.state='NC'
+    }
+    if (!req.body.pic) {
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+
+    places[id] = req.body
+    res.redirect(`/places/${id}`)
+}
+})
 
 router.delete('/:id', (req, res) => {
     let id = Number(req.params.id)
