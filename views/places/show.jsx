3e3286fccfd70ad73f5d8eaf59d162755549ca4b
Andrew Thomas <athomas94@gmail.com>
2024-02-03 22:48:03 -0500

    added new text to the show pages

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,12 +5,24 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{data.place.name}</h1>
+            <div className="row">
+                <div className="col-sm-6">
+                    <h1>{data.place.name}</h1>
+                    <br />
+                    <img src={data.place.pic} alt={data.place.state} />
+                    <h3>
+                        Located in {data.place.city}, {data.place.state}
+                    </h3>
+                </div>
+            <div className="col-sm-6">
             <div>
-                <h2>Description</h2>
-                {data.place.city}, {data.place.state}
-                <br />
-                {data.place.cuisines}
+                <h1>Description</h1>
+                <h3>
+                    {data.place.showEstablished()}
+                </h3>
+                <h4>
+                    Serving {data.place.cuisines}
+                </h4>
             </div>
             <div>
             <h2>Rating</h2>
@@ -29,6 +41,8 @@ function show (data) {
             <div>
                 <h2>Comments</h2>
                 No comments yet.
+            </div>
+            </div>
             </div>
           </main>
         </Def>
