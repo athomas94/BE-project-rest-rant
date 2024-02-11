04e4288f4c79b2a11d9cb070e2deb67815a333a1
Andrew Thomas <athomas94@gmail.com>
2024-02-10 23:33:05 -0500

    added edit routes and functionality

--- controllers/places.js
+++ controllers/places.js
@@ -44,11 +44,25 @@ router.get('/:id', (req, res) => {
 })
 
 router.get('/:id/edit', (req, res) => {
-    res.send('GET edit form stub')
- })
+    db.Place.findById(req.params.id)
+    .then(place => {
+        res.render('places/edit', { place })
+    })
+    .catch(err => {
+        res.render('error404')
+    })
+})
+
 
 router.put('/:id', (req, res) => {
-    res.send('PUT /places/:id stub')
+    db.Place.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+        res.redirect(`/places/${req.params.id}`)
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.render('error404')
+    })
 })
 
 router.delete('/:id/rant/:rantID', (req, res) => {
@@ -56,8 +70,16 @@ router.delete('/:id/rant/:rantID', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-    restart.send('DELETE /places/:id stub')
- })
+    db.Place.findByIdAndDelete(req.params.id)
+    .then(place => {
+        res.redirect('/places')
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.render('error404')
+    })
+})
+
 
 router.post('/:id/rant', (req, res) => {
     res.send('GET /places/:id/rant stub')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -16,10 +16,14 @@ function show (data) {
         let sumRatings = data.place.comments.reduce((tot, c) => {
             return tot + c.stars
         }, 0)
-        let averageRating = sumRatings / data.place.comments.length 
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+            stars += '⭐'
+        }
         rating = (
             <h3>
-                {Math.round(averageRating)} stars
+                {stars} stars
             </h3>
         )
         comments = data.place.comments.map(c => {
@@ -61,11 +65,11 @@ function show (data) {
             <h2>Rating</h2>
                 {rating}
                 <br />
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
                     Edit
                 </a>
                 <br />
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
                     <button type="submit" className="btn btn-danger">
                         Delete
                     </button>
